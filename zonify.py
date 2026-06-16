717c8183e3a269d750c770e0c53c1fb6069caec6
joaquinagh2-source <joaquin.agh2@gmail.com>
2026-06-16 17:51:33 +0000

    Implementacion del FLask, lista de cache e importacion de la funcion para obtener los eventos de cosevi

--- zonify.py
+++ zonify.py
@@ -1,4 +1,6 @@
-import flask #Framework que se utiliza para crear aplicaciones we de manera sencillas para que python pueda responder peticiones web
-import urllib.request #Sirve para enviar solicitudes HTTP y recibbir las respuestas de esta de forma sencilla
-import xml.etree.ElementTree #Hace que los datos XML que otorgan las RSS sean mas faciles de transformar en texto limpio
-import datetime #Permite agarrar los textos raros de fechas que mandan los periódicos y transformarlos a formatos limpios
+from flask import Flask, render_template, redirect, url_for #El flask es lo que deja crear el servidor web, el render template es lo que comina los archivos html y combinarlos con los datos que recibe python para mostrarlos bien al usuario y el redirect y el urlfor sirven para que si pasa un evento, lo mande rapido a la pagina.
+from services.cosevi_service import obtener_eventos_cosevi #esta linea hace que del codigo de cosevi service, generado junto al api publico de cosevi y ayuda de ia, jale la definicion que se usa para otener los datos de la api 
+
+app = Flask(__name__) #basicamente esta linea sirve para que el servidor web exista
+eventos_cache = [] #Es una lista vacia en la RAM que es para guardar datos temporalmente y hacer la web mas rapida
+
