3dc5dafa8252f0a0f30525e9a826955c188fbc09
joaquinagh2-source <joaquin.agh2@gmail.com>
2026-06-18 19:19:04 +0000

    Agrego nueva fuente de informacion de la cnfl

--- zonify.py
+++ zonify.py
@@ -1,30 +1,58 @@
 from flask import Flask, render_template, redirect, url_for, request #El flask es lo que deja crear el servidor web, el render template es lo que comina los archivos html y combinarlos con los datos que recibe python para mostrarlos bien al usuario y el redirect y el urlfor sirven para que si pasa un evento, lo mande rapido a la pagina.
 from services.cosevi_service import obtener_eventos_cosevi #esta linea hace que del codigo de cosevi service, generado junto al api publico de cosevi y ayuda de ia, jale la definicion que se usa para otener los datos de la api 
+from services.cnfl_averias_service import obtener_averias_cnfl
 from geopy.geocoders import Nominatim
 
 app = Flask(__name__) #basicamente esta linea sirve para que el servidor web exista
 eventos_cache = [] #Es una lista vacia en la RAM que es para guardar datos temporalmente y hacer la web mas rapida
+averias_cache = []
+
 
 @app.route("/") #apenas el usuario entre, esto hace que se ejecute la funcion de inicio, la cual tiene que ver con lo que ve el usuario en el html
 def inicio():
     global eventos_cache #Le avisa a python que vamos a usar la lista creada anterior mente al ejecutar la funcion
+    global averias_cache
 
-    if not eventos_cache: #Esta linea lo que hace es preguntar si la lista esta vacia o no, si la respuesta es si va, y jala la funcion de obtener datos cosevi
+    if not eventos_cache: #Esta linea lo que hace es preguntar si la lista esta vacia o no, si la respuesta es si va, y jala la funcion de obtener datos y averias
         eventos_cache = obtener_eventos_cosevi()
+    if not averias_cache:
+        averias_cache = obtener_averias_cnfl()
 
-    return render_template( # Esto es lo que hace que se le muestre al usuario. Toma html y le manda la lista de accidentes que tenemos guardada o de la api de cosevi 
+    clientes_afectados = sum
+    (averia.get("clientes_afectados") or 0 
+    for averia in averias_cache) #Esta linea hace una suma de todos los clientes afectados por las averias, para mostrarlo en el html
+
+
+
+
+    return render_template( # Esto es lo que hace que se le muestre al usuario. Toma html y le manda la lista de accidentes y averias que tenemos guardada  
         "index.html",
         eventos=eventos_cache
+    )
+
+    return render_template(
+        "index.html",
+        averias=averias_cache,
+        clientes_afectados=clientes_afectados,
     )
 
 
 @app.route("/actualizar") #Basicamente esto sirve para actualizar los datos una vez el usuario ya lleva cierto tiempo en la pagina
 def actualizar():
     global eventos_cache
+    global averias_cache
 
-    eventos_cache = obtener_eventos_cosevi()
+    eventos_cache = obtener_eventos_cosevi() 
+
+    try:#Verificar si funciona y si no, mostrar error pero sin que se caiga la pagina
+        averias_cache = obtener_averias_cnfl()
+    except RuntimeError as e:
+        averias_cache = []
+        print("Error al actualizar averias de CNFL:", e)
 
     return redirect(url_for("inicio"))
+
+
 
 
 @app.route("/ubicacion")
